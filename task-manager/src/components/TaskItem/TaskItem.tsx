73a997ff951c7dc77dfdc2b6eb97222fd8fa1a04
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-23 00:14:12 -0500

    added delete button

--- task-manager/src/components/TaskItem/TaskItem.tsx
+++ task-manager/src/components/TaskItem/TaskItem.tsx
@@ -1,19 +1,21 @@
-import type { Task, TaskStatus } from '../../types';
-import type { TaskItemProps } from '../../types';
+import type { Task, TaskStatus } from "../../types";
+import type { TaskItemProps } from "../../types";
 
 export const TaskItem = ({ task, onStatusChange, onDelete }: TaskItemProps) => {
-    return (
-        <h3>{task.title}</h3>
-        <p>{task.description}</p>
-        <p>Priority: {task.priority}</p>
-        <p>Due: {task.dueDate}</p>
-        
-        <select name="status" id="task.id" value='task.status' onChange=onStatusChange>
+  return (
+    <>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <p>Priority: {task.priority}</p>
+      <p>Due: {task.dueDate}</p>
+
+      <select id={task.id} value={task.status} onChange={() => onStatusChange}>
         <option value="pending">Pending</option>
-        <option value="in-progress">In Progess</option>
-                <option value="completed">completed</option>
+        <option value="in-progress">In Progress</option>
+        <option value="completed">completed</option>
+      </select>
 
-        </select>
-    )
-}
-
+      <button onClick={() => onDelete}>Delete</button>
+    </>
+  );
+};
