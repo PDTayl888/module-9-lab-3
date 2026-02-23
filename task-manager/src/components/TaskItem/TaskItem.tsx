8ef0205c6ef4954330cace19667d97d15889e7a7
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-22 23:43:20 -0500

    started task item

--- task-manager/src/components/TaskItem/TaskItem.tsx
+++ task-manager/src/components/TaskItem/TaskItem.tsx
@@ -1,1 +1,12 @@
+import type { Task, TaskStatus } from '../../types';
+import type { TaskItemProps } from '../../types';
+
+export const TaskItem = ({ task, onStatusChange, onDelete }: TaskItemProps) => {
+    return (
+        <h3>{task.title}</h3>
+        <p>{task.description}</p>
+        <p>Priority: {task.priority}</p>
+        <p>Due: {task.dueDate}</p>
+    )
+}
 
