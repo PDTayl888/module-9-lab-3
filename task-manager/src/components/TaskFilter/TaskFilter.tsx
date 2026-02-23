f3dfb7ad3b13795e1d18ed289af060621adfa021
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-23 00:27:58 -0500

    started filter component

---
+++ task-manager/src/components/TaskFilter/TaskFilter.tsx
@@ -0,0 +1,13 @@
+
+
+export const TaskFilter = ({onFilterChange}: TaskFilterProps) => {
+    return(
+        <>
+        <label>Status:</label>
+        <select name="" id=""></select>
+
+        <label>PRiority</label>
+        <select name="" id=""></select>
+        </>
+    )
+}
